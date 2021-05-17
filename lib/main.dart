eae2226cad8dbc15d6cb04fde4df996a2aedc2da
anas_altarazi <anas.anas1998,tar@gmail.com>
2021-05-17 18:18:02 +0300

    -- divider & tabBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:example1/home.dart';
+import 'package:example1/radio_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -8,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: Home(),
+      home: RadioPage(),
     );
   }
 }
