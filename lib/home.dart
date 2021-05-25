9f97756ce5108a95f7ad117ff56c413d5d6579ed
anas_altarazi <anas.anas1998,tar@gmail.com>
2021-05-25 16:43:45 +0300

    -- build home page

--- lib/home.dart
+++ lib/home.dart
@@ -1,67 +1,42 @@
-import 'package:example1/inh_widget.dart';
-import 'package:example1/profile.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
-class Home extends StatefulWidget {
-  Home({Key key}) : super(key: key);
-  @override
-  _HomeState createState() => _HomeState();
-}
+class Home extends StatelessWidget {
+  const Home({Key key}) : super(key: key);
 
-class _HomeState extends State<Home> {
-  final emailController = TextEditingController();
-  final passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Padding(padding: EdgeInsets.all(8)),
-            Center(child: Text('form')),
-            Padding(padding: EdgeInsets.all(8)),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: TextField(
-                controller: emailController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'email',
-                ),
-              ),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Demo App'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'E-mail'),
             ),
-            Padding(padding: EdgeInsets.all(8)),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: TextField(
-                controller: passwordController,
-                obscureText: true,
-                style: TextStyle(),
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                  labelStyle: TextStyle(),
-                ),
-              ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'password'),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  Get.put(emailController.text, tag: 'email');
-                  BaseWidget.of(context).password = passwordController.text;
-                  Get.to(
-                    () => Profile(),
-                  );
-                },
-                child: Text('data'),
-              ),
-            )
-          ],
-        ),
+          ),
+          Container(
+            padding: EdgeInsets.all(8.0),
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text('Sign In'),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,19 @@
 import 'package:example1/home.dart';
-import 'package:example1/inh_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
-void main() => runApp(MyApp());
+main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return BaseWidget(
-      child: GetMaterialApp(
-        title: 'Material App',
-        home: Home(),
-      ),
+    return MaterialApp(
+      routes: {'/home': (_) => Home(), '/profile': null},
+      initialRoute: '/home',
+      theme: ThemeData(primarySwatch: Colors.indigo),
     );
   }
 }
